854e2fbb6ab62f05a17eb4c05ed5eb17274bac52
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:00:38 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Dissonance" `
+  -ModName "Dissonance" `
+  -ModFolder "Dissonance" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/dissonance/Dissonance-Original.zip" `
+  -ModBaseFilesUrlHash "6055210e887305ec1b9ddc9883bd75ebbfb4517d940041687a310dc3bc1b1c1d" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
